10f802cee1acb42f0b35eca28c5d6437183e4ad5
mimee29 <mirleed29@gmail.com>
2024-10-28 08:57:57 -0400

    Update app.service.ts

--- src/app.service.ts
+++ src/app.service.ts
@@ -3,6 +3,6 @@ import { Injectable } from '@nestjs/common';
 @Injectable()
 export class AppService {
   getHello(): string {
-    return 'Gestion projet TechDuo en nestjs!';
+    return 'Gestion projet en nestjs!';
   }
 }
